1b6753afc21dfdfed04e21347f91ec16c2cbd40b
Sequømics <sequomics@gmail.com>
2015-12-01 21:54:07 +0530

    Initial commit
    Help: https://github.com/gruntjs or http://gruntjs.com/

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,1 +1,19 @@
 #!/usr/bin/env node
+
+
+/*!
+ * Legal® [v1.0.0]
+ * ______________________________________________________________
+ * Grunt, http://gruntjs.com/
+ * The JavaScript Task Runner.
+ * ______________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ______________________________________________________________
+ * @date      : 01-Dec-2015
+ * @require   : Node.js®
+ * @require   : NPM
+ * ______________________________________________________________
+ */
