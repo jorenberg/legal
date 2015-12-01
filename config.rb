63a82cb1db7a1ddb749c80989c26c39066b22fa5
Sequømics <sequomics@gmail.com>
2015-12-01 20:15:56 +0530

    Initial commit
    Project “PATH” settings.
    @root: "source/core/".

--- config.rb
+++ config.rb
@@ -17,3 +17,10 @@
 
 # Require compass plugin:
 require 'compass/import-once/activate'
+
+# Setting the root path of project:
+http_path       = "/"
+css_dir         = "source/core/stylesheets"
+sass_dir        = "source/core/sass"
+images_dir      = "source/core/images"
+javascripts_dir = "source/core/javascripts"
