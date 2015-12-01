31b7184daaeb49f6c04653a1e764d6935fd8971c
Sequømics <sequomics@gmail.com>
2015-12-01 21:39:57 +0530

    Initial commit
    To enable relative paths to assets.
    Usage: true

--- config.rb
+++ config.rb
@@ -27,3 +27,6 @@ javascripts_dir = "source/core/javascripts"
 
 # Preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
+
+# To enable relative paths to assets via compass helper functions:
+relative_assets = true
