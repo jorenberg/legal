c29eabdb026eb309c6184a7c948616e71017c55e
Sequømics <sequomics@gmail.com>
2015-12-01 20:11:09 +0530

    Initial commit
    Help: http://compass-style.org/

--- config.rb
+++ config.rb
@@ -1,1 +1,16 @@
 #!/usr/bin/env ruby
+
+
+# AXIX®-Pro
+# Legal Docs of Sequømics Corporation.
+# @author     : Prabhat Kumar
+# @copyright  : Sequømics Corporation
+# @homepage   : http://sequomics.com/en/legal/
+# @date       : 01-Dec-2015
+# @require    : Ruby
+# @require    : Sass
+# @require    : Compass
+# @version    : 0.0.1
+# ---------------------------------------
+# Principle  : Standard Module Definition
+# ---------------------------------------
