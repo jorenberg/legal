f4384808c49f04998358f1093d30f7ea71c75bca
Sequømics <sequomics@gmail.com>
2015-12-03 00:08:44 +0530

    Initial commit
    "set -e" causes the shell to "exit" if any sub-command or pipeline returns a "non-zero status".

--- install.sh
+++ install.sh
@@ -4,3 +4,6 @@
 # Handcrafted by Prabhat Kumar, http://prabhatkumar.org/.
 # Copyright © 2014 - 2015, Sequømics Corporation. All rights reserved.
 # ====================================================================
+
+# To exit whenever anything returns a non-zero return value.
+set -e
