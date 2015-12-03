afc63f352dfe864833f604df321be1efa4106ae8
Sequømics <sequomics@gmail.com>
2015-12-03 17:44:18 +0530

    Initial commit
    Grunt Task(s) defined for Legal®.

--- Gruntfile.js
+++ Gruntfile.js
@@ -66,3 +66,10 @@
 // ====================================
 // @method    : grunt.loadNpmTasks(pluginName)
 // @method    : grunt.registerTask(task-name)
+
+// Grunt Task(s):-----------------------------------------
+// 1. Task for "Linter" - HTML, SCSS, CSS and JavaScript,
+// 2. Task for "Minifier" - HTML and CSS,
+// 3. Task for "clean files and folders",
+// 4. Task for "Assemble": Default, and
+// 5. Task for "Travis CI", using ".travis.yml" from root.
