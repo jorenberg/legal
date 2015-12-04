9b8c9ad02953c777c56979c3f4959c73474c9393
Sequømics <sequomics@gmail.com>
2015-12-04 22:34:20 +0530

    Initial commit
    Copyright © 2014 - 2015, Sequømics Corporation. All rights reserved.

--- source/core/fonts/compare.py
+++ source/core/fonts/compare.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python
+
+
+__company__ 	= 'Sequømics Corporation'
+__homepage__ 	= 'http://sequomics.com/'
+__account__ 	= 'sequomics'
+__githubURL__ 	= 'https://github.com/sequomics'
+__authors__ 	= [
+    '"Prabhat Kumar" <prabhat.genome@gmail.com>',
+    '"Sequømics Corporation" <admin@sequomics.com>'
+    ]
+__license__     = 'Apache License'
